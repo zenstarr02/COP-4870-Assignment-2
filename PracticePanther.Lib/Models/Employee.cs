2918b767f4893a4a24e8c6fd8398b09ce50ab3f6
zenstarr02 <135061330+zenstarr02@users.noreply.github.com>
2023-07-29 22:05:56 -0400

    Add files via upload

---
+++ PracticePanther.Lib/Models/Employee.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticePanther.Lib.Models
+{
+    public class Employee
+    {
+        private static int LastId = 0;
+
+        Employee() { Name = string.Empty; Id = ++LastId; }
+        public string Name { get; set; }
+        public int Id { get; private set;}
+        public float Rate { get; set; }
+
+    }
+}
---
+++ PracticePanther.Lib/Models/Time.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticePanther.Lib.Models
+{
+    public class Time
+    {
+        public DateTime Date {get; set;}
+
+        public string Narrative { get; set;}
+
+        public int Hours { get; set;}
+
+        public int EmployeeId { get; set; }
+
+    }
+}
